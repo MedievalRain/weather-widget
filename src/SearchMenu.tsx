7b2741240d26a42690082568cce88303cfceee21
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 01:26:42 +0300

    Fetch cities

--- src/SearchMenu.tsx
+++ src/SearchMenu.tsx
@@ -5,6 +5,18 @@ import { City } from "./types";
 function SearchMenu() {
   const [isOpened, setIsOpened] = useState(false);
   const [cities, setCities] = useState<City[]>([]);
+  async function fetchCities(cityQuery: string) {
+    if (true) {
+      const response = await fetch(
+        `https://weather-api-flax-eta.vercel.app/api/search?city=${cityQuery}`
+      );
+      const data: unknown = await response.json();
+      if (response.status === 200 && Array.isArray(data)) {
+        setCities(data);
+      }
+    }
+  }
+
   return (
     <>
       <button
@@ -21,6 +33,9 @@ function SearchMenu() {
             className="bg-slate-700 p-2 rounded-t-md outline-slate-900 outline-1"
             type="text"
             placeholder="City..."
+            onChange={(event) => {
+              fetchCities(event.target.value).catch(console.error);
+            }}
           />
           <ul className="bg-slate-700">
             {cities.map((city) => (
