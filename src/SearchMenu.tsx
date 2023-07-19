2ef526fa665bd2462ca8f1b135fe30041c1d2d24
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 00:15:58 +0300

    Rename variable to be more obvious

--- src/SearchMenu.tsx
+++ src/SearchMenu.tsx
@@ -2,12 +2,12 @@ import SearchIcon from "./icons/SearchIcon";
 import { useState } from "react";
 
 function SearchMenu() {
-  const [opened, setOpened] = useState(false);
+  const [isOpened, setIsOpened] = useState(false);
   return (
     <>
       <button
         onClick={() => {
-          setOpened(!opened);
+          setIsOpened(!isOpened);
         }}
         className="bg-slate-900 rounded-full p-3 fill-white place-self-end"
       >
