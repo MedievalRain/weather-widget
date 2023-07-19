988b7bf774b7ec220444598c08a0d466385800cd
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 00:37:05 +0300

    Add example dropdown layout

--- src/SearchMenu.tsx
+++ src/SearchMenu.tsx
@@ -3,6 +3,7 @@ import { useState } from "react";
 
 function SearchMenu() {
   const [isOpened, setIsOpened] = useState(false);
+
   return (
     <>
       <button
@@ -13,6 +14,20 @@ function SearchMenu() {
       >
         <SearchIcon />
       </button>
+      {isOpened ? (
+        <div className="absolute right-14 top-2">
+          <input
+            className="bg-slate-700 p-2 rounded-t-md outline-slate-900 outline-1"
+            type="text"
+            placeholder="City..."
+          />
+          <ul className="bg-slate-700">
+            <li className="p-1">
+              <button></button>
+            </li>
+          </ul>
+        </div>
+      ) : null}
     </>
   );
 }
