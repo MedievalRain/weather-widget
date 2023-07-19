0f39b943d7b73893ae1076c95344225ffa427bfc
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 02:34:05 +0300

    Add and style location section

--- src/Display.tsx
+++ src/Display.tsx
@@ -1,3 +1,4 @@
+import LocationIcon from "./icons/LocationIcon";
 import { City } from "./types";
 
 interface IDisplayProps {
@@ -5,7 +6,15 @@ interface IDisplayProps {
 }
 
 function Display({ pickedCity }: IDisplayProps) {
-  return <div>{pickedCity.city}</div>;
+  return (
+    <div className="flex flex-col divide-y w-full px-2 divide-teal-100">
+      <div className="h-72"></div>
+      <div className="inline-flex items-center gap-1 fill-white px-2">
+        <LocationIcon />
+        {pickedCity.city}, {pickedCity.country}
+      </div>
+    </div>
+  );
 }
 
 export default Display;
