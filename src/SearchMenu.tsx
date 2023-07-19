ef6b0510fb03cac80579178c26ccdceffda47f17
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 00:14:30 +0300

    Change menu state variable on click

--- src/SearchMenu.tsx
+++ src/SearchMenu.tsx
@@ -1,10 +1,19 @@
 import SearchIcon from "./icons/SearchIcon";
+import { useState } from "react";
 
 function SearchMenu() {
+  const [opened, setOpened] = useState(false);
   return (
-    <button className="bg-slate-900 rounded-full p-3 fill-white place-self-end">
-      <SearchIcon />
-    </button>
+    <>
+      <button
+        onClick={() => {
+          setOpened(!opened);
+        }}
+        className="bg-slate-900 rounded-full p-3 fill-white place-self-end"
+      >
+        <SearchIcon />
+      </button>
+    </>
   );
 }
 
