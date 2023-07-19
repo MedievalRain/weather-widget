3ec048a047e1497b4c43a26c3ca707fd42a47cde
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 01:50:44 +0300

    Add default picked city

--- src/Widget.tsx
+++ src/Widget.tsx
@@ -1,7 +1,14 @@
 import { useState } from "react";
 import SearchMenu from "./SearchMenu";
+import { City } from "./types";
 
 function Widget() {
+  const [pickedCity, setPickedCity] = useState<City>({
+    city: "Moscow",
+    latitude: "55.755800",
+    longitude: "37.617800",
+    country: "Russia",
+  });
   return (
     <div className="gradient-bg text-white max-w-md w-full rounded-md h-96">
       <div className="h-full w-full backdrop-blur-3xl flex flex-col items-start p-2">
