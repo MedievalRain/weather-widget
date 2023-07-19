ba0021c2458dc6ed44814761882fb351b8086057
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-19 23:46:11 +0300

    Center widget for conviniet development

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,9 @@ import Widget from "./Widget";
 
 function App() {
   return (
-    <>
+    <div className="flex items-center justify-center h-screen">
       <Widget />
-    </>
+    </div>
   );
 }
 
