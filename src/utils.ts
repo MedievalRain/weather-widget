a5aebf53ee7aed482fa996cfc72af318c3c81bdc
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 09:26:40 +0300

    Replace concatenation with template literals

--- src/utils.ts
+++ src/utils.ts
@@ -1,8 +1,5 @@
 export function getSrc(filename: string, isDay: number) {
-  return (
-    "/weather/" +
-    filename +
-    (isDay === 0 && filename == "clear" ? "-night" : "") +
-    ".svg"
-  );
+  return `/weather/${filename}${
+    isDay === 0 && filename == "clear" ? "-night" : ""
+  }.svg`;
 }
