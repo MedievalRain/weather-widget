418e5df9c38166d17c28759625ebff2052d358ac
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 05:04:36 +0300

    Rename WeatherResponse type to WeatherData

--- src/types.ts
+++ src/types.ts
@@ -11,3 +11,21 @@ export type WwCode = {
   text: string;
   file: string;
 };
+
+export type WeatherData = {
+  latitude: number;
+  longitude: number;
+  generationtime_ms: number;
+  utc_offset_seconds: number;
+  timezone: string;
+  timezone_abbreviation: string;
+  elevation: number;
+  current_weather: {
+    temperature: number;
+    windspeed: number;
+    winddirection: number;
+    weathercode: number;
+    is_day: number;
+    time: string;
+  };
+};
