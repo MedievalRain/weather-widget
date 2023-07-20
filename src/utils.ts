acce6aee87d884a44a7ea58c209c6cb3fadf1151
Vladislav <31704441+MedievalRain@users.noreply.github.com>
2023-07-20 05:38:25 +0300

    Add function to generate icon file path

---
+++ src/utils.ts
@@ -0,0 +1,8 @@
+export function getSrc(filename: string, isDay: number) {
+  return (
+    "/weather/" +
+    filename +
+    (isDay === 0 && filename == "clear" ? "-night" : "") +
+    ".svg"
+  );
+}
